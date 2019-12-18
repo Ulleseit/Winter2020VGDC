dec826dda239e5734daeb519b69eae0cfe105ec7
Ulleseit <ulleseit99@gmail.com>
2019-12-18 01:22:14 -0800

    Character Selection and Movement
    Enabled character to select and move characters on the board.

---
+++ Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void move(float x, float y)
+    {
+      this.transform.position = new Vector3(x, y, 0);
+    }
+}
