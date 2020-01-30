c844dea21e0e867355965622b386b595bc980446
Henry Feng <usaruner@gmail.com>
2020-01-29 18:17:03 -0800

    UI updates to Scenes
    Menu:
    Start button is done,
    Credits Button is done,
    Exit Button is done,
    
    overworld_map:
    Dumby map added;
    Menu button added;
    
    Credits Scene:
    Credits text added
    Menu button added

---
+++ Assets/Scripts/UI/CreditsPressed.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsPressed : MonoBehaviour
+{
+	public void toCredits(){
+		SceneManager.LoadScene("Credits");
+	}
+}
---
+++ Assets/Scripts/UI/StartPressed.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartPressed : MonoBehaviour
+{
+    public void nextScene()
+    {
+        SceneManager.LoadScene("Overworld_Map");
+    }
+}
