698ba3e649533e16b42dc95f4d657a1a75a56a70
Ulleseit <ulleseit99@gmail.com>
2020-01-31 12:11:50 -0800

    Overworld node movement
    Can now place tiles through tile palette on overworld, when player clicks on a node, the camera is moved to where that player is.

---
+++ Assets/Scripts/Overworld/MoveToNode.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MoveToNode : MonoBehaviour
+{
+    Tilemap tilemap;
+    TileBase tile;
+    // Start is called before the first frame update
+    void Start()
+    {
+      tilemap = this.GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Input.GetMouseButtonDown(0))
+      {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.x = (float)(System.Math.Floor(pos.x)+.5);
+        pos.y = (float)(System.Math.Floor(pos.y)+.5);
+        pos.z = 0;
+        Vector3Int tileLocation = tilemap.WorldToCell(pos);//0,0 is the starting node location
+        tile = tilemap.GetTile(tileLocation);
+        if(Equals(tile.name, "Node"))
+        {
+          pos.z = -10;
+          Camera.main.transform.position = (pos);
+        }
+      }
+    }
+}
