e3269e62082c01f479c6c39baeadffcc08ddf59d
Rafferty Chen <raffertychen@gmail.com>
2020-02-21 16:06:11 -0800

    Add Item type

---
+++ Assets/Scripts/UI/Item.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+
+    public enum ItemType
+    {
+        Weapon,
+        HeadArmor,
+        ChestArmor,
+        LegArmor,
+        Boots,
+        Potion
+    }
+
+    public ItemType itemType;
+    public int amount;
+    public string name;
+
+}
