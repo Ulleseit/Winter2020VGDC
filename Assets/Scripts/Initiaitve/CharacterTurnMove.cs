35a79d89924763f67bd0f4f91997ad256628f677
Ulleseit <ulleseit99@gmail.com>
2020-03-10 02:15:54 -0700

    Enemy moves and attacks
    Big spider not working since it takes multiple squares, and enemies shift around PCs when they do not have to move, because they count the square they are in as occupied.

--- Assets/Scripts/Initiaitve/CharacterTurnMove.cs
+++ Assets/Scripts/Initiaitve/CharacterTurnMove.cs
@@ -9,6 +9,7 @@ public class CharacterTurnMove : MonoBehaviour
     GameObject[] characters;
     GameObject selected = null;
     GameObject[] enemies;
+	GameObject[] combatMembers;
   	Tilemap tilemap;
   	public GameObject[] buttons;
   	bool buttonPressed = false;
@@ -41,7 +42,7 @@ public class CharacterTurnMove : MonoBehaviour
     if(!moving)
     {
 		Camera.main.transform.position = new Vector3(Mathf.Clamp(Camera.main.transform.position.x, minX, maxX), Mathf.Clamp(Camera.main.transform.position.y, minY, maxY), -10);
-		GameObject[] combatMembers = createCombatMembers();
+		combatMembers = createCombatMembers();
 		selected = combatMembers[0];
 		Vector3 position = createTileMouse();
 		if(cameraLock)
@@ -188,23 +189,39 @@ public class CharacterTurnMove : MonoBehaviour
 		}
 		else if(selected.tag == "Enemy")
 		{
-      foreach(GameObject button in buttons)
-      {
-        button.SetActive(false);
-      }
+			foreach(GameObject button in buttons)
+			{
+				button.SetActive(false);
+			}
 			float enemySquare = 10000f;
-      Vector3 enemyPosition = new Vector3(0,0,0);
-      foreach(GameObject c in characters)
-      {
-        if((Mathf.Abs(c.GetComponent<Transform>().position.x-selected.GetComponent<Transform>().position.x)+Mathf.Abs(c.GetComponent<Transform>().position.y-selected.GetComponent<Transform>().position.y)) < enemySquare)
-        {
-          enemySquare = (Mathf.Abs(c.GetComponent<Transform>().position.x-selected.GetComponent<Transform>().position.x)+Mathf.Abs(c.GetComponent<Transform>().position.y-selected.GetComponent<Transform>().position.y));
-          enemyPosition = c.GetComponent<Transform>().position;
-        }
-      }
-      //Check if any squares next to the thing are occupied, starting with the closest one
-      List<Point> p = stepTowardsPath(enemyPosition.x, enemyPosition.y);
-      StartCoroutine(stepThroughPath(p));
+			Vector3 enemyPosition = new Vector3(0,0,0);
+			foreach(GameObject c in characters)
+			{
+				if((Mathf.Abs(c.GetComponent<Transform>().position.x-selected.GetComponent<Transform>().position.x)+Mathf.Abs(c.GetComponent<Transform>().position.y-selected.GetComponent<Transform>().position.y)) < enemySquare)
+				{
+				  enemySquare = (Mathf.Abs(c.GetComponent<Transform>().position.x-selected.GetComponent<Transform>().position.x)+Mathf.Abs(c.GetComponent<Transform>().position.y-selected.GetComponent<Transform>().position.y));
+				  enemyPosition = c.GetComponent<Transform>().position;
+				}
+			  }
+			
+			if(checkOccupancy(new Vector3(enemyPosition.x+1, enemyPosition.y, enemyPosition.z)))
+			{
+				enemyPosition = new Vector3(enemyPosition.x+1, enemyPosition.y, enemyPosition.z);
+			}
+			else if(checkOccupancy(new Vector3(enemyPosition.x-1, enemyPosition.y, enemyPosition.z)))
+			{
+				enemyPosition = new Vector3(enemyPosition.x-1, enemyPosition.y, enemyPosition.z);
+			}
+			else if(checkOccupancy(new Vector3(enemyPosition.x, enemyPosition.y+1, enemyPosition.z)))
+			{
+				enemyPosition = new Vector3(enemyPosition.x, enemyPosition.y+1, enemyPosition.z);
+			}
+			else if(checkOccupancy(new Vector3(enemyPosition.x, enemyPosition.y-1, enemyPosition.z)))
+			{
+				enemyPosition = new Vector3(enemyPosition.x, enemyPosition.y-1, enemyPosition.z);
+			}
+			List<Point> p = stepTowardsPath(enemyPosition.x, enemyPosition.y);
+			StartCoroutine(stepThroughPath(p));
 			selected.GetComponent<Character>().reduceInitiative();
 		}
     }
@@ -582,20 +599,60 @@ public class CharacterTurnMove : MonoBehaviour
 		foreach(Point p in path)
 		{
 			selected.GetComponent<Transform>().position = new Vector3(p.X, p.Y, 0f);
-      selected.GetComponent<Character>().reduceActionPoints(1);
-      if(cameraLock)
-  		{
-  		  Camera.main.transform.position = new Vector3(Mathf.Clamp(selected.GetComponent<Transform>().position.x, minX, maxX), Mathf.Clamp(selected.GetComponent<Transform>().position.y, minY, maxY), -10);
-  		  Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, -10);
-  		}
-      yield return new WaitForSeconds(.25f);
+		  selected.GetComponent<Character>().reduceActionPoints(1);
+		  if(cameraLock)
+			{
+			  Camera.main.transform.position = new Vector3(Mathf.Clamp(selected.GetComponent<Transform>().position.x, minX, maxX), Mathf.Clamp(selected.GetComponent<Transform>().position.y, minY, maxY), -10);
+			  Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, -10);
+			}
+			yield return new WaitForSeconds(.25f);
 		}
     if(selected.tag == "Enemy")
     {
-      selected.GetComponent<Character>().currentActionPoints = selected.GetComponent<Character>().maxActionPoints;
+		if(selected.GetComponent<Character>().currentActionPoints >= 2 && !checkCharacterOccupancy(new Vector3(selected.transform.position.x+1, selected.transform.position.y, selected.transform.position.z)).Item1)
+		{
+			checkCharacterOccupancy(new Vector3(selected.transform.position.x+1, selected.transform.position.y, selected.transform.position.z)).Item2.GetComponent<Character>().currentHealth--;
+		}
+		else if(selected.GetComponent<Character>().currentActionPoints >= 2 && !checkCharacterOccupancy(new Vector3(selected.transform.position.x-1, selected.transform.position.y, selected.transform.position.z)).Item1)
+		{
+			checkCharacterOccupancy(new Vector3(selected.transform.position.x-1, selected.transform.position.y, selected.transform.position.z)).Item2.GetComponent<Character>().currentHealth--;
+		}
+		else if(selected.GetComponent<Character>().currentActionPoints >= 2 && !checkCharacterOccupancy(new Vector3(selected.transform.position.x, selected.transform.position.y+1, selected.transform.position.z)).Item1)
+		{
+			checkCharacterOccupancy(new Vector3(selected.transform.position.x, selected.transform.position.y+1, selected.transform.position.z)).Item2.GetComponent<Character>().currentHealth--;
+		}
+		else if(selected.GetComponent<Character>().currentActionPoints >= 2 && !checkCharacterOccupancy(new Vector3(selected.transform.position.x, selected.transform.position.y-1, selected.transform.position.z)).Item1)
+		{
+			checkCharacterOccupancy(new Vector3(selected.transform.position.x, selected.transform.position.y-1, selected.transform.position.z)).Item2.GetComponent<Character>().currentHealth--;
+		}
+		
+		selected.GetComponent<Character>().currentActionPoints = selected.GetComponent<Character>().maxActionPoints;
     }
     moving = false;
 	}
 
+	bool checkOccupancy(Vector3 checkPosition)
+	{
+		foreach(GameObject s in combatMembers)
+		{
+			if(s.transform.position == checkPosition)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	Tuple<bool,GameObject> checkCharacterOccupancy(Vector3 checkPosition)
+	{
+		foreach(GameObject s in characters)
+		{
+			if(s.transform.position == checkPosition)
+			{
+				return Tuple.Create(false, s);
+			}
+		}
+		return Tuple.Create(true, selected);
+	}
 
 }
